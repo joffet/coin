f4dd7b074fc6f92e88dffc4597bac1869fd2dea8
Jason Offet <joffet@venue.live>
2024-11-10 18:18:55 -0500

    setup basic table, state, and fetch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [coinData, setCoinData] = useState([]);
+
+  useEffect(() => {
+    const getApi = async () => {
+      const response = await fetch("localhost:4000/get");
+      const responseObj = await response.json();
+      setCoinData(responseObj.data);
+    };
+
+    getApi();
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table>
+        <thead>
+          <th scope="col">Name</th>
+          <th scope="col">Price</th>
+          <th scope="col">Market Cap</th>
+          <th scope="col">Circulating Supply</th>
+          <th scope="col">Change &</th>
+          <th scope="col">Last (24H)</th>
+        </thead>
+        <tbody></tbody>
+      </table>
     </div>
   );
 }
