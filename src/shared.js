02d8c326fc2a26d8713699e7d1dfe73058d2f845
Jason Offet <joffet@venue.live>
2024-11-10 20:13:34 -0500

    added test for helpers and found bug

--- src/shared.js
+++ src/shared.js
@@ -1,13 +1,26 @@
 export const roundToDecimal = (num) => {
   const string = (Math.round(num * 100) / 100).toString();
   const split = string.split(".");
+
+  const beforeDecimalOld = split[0];
+  let beforeDecimalNew = "";
+  for (let index = beforeDecimalOld.length - 1; index >= 0; index--) {
+    const insertComma =
+      (beforeDecimalOld.length - index) % 3 === 0 && // 3 nums behind it
+      index < beforeDecimalOld.length && // not the first run
+      index !== 0; // not the last run
+    beforeDecimalNew = `${insertComma ? "," : ""}${
+      beforeDecimalOld[index]
+    }${beforeDecimalNew}`;
+  }
+
   let afterDecimal = split[1] || "00";
   if (afterDecimal.length === 1) afterDecimal += "0";
-  return `${split[0]}.${afterDecimal}`;
+  return `${beforeDecimalNew}.${afterDecimal}`;
 };
 
 export const displayNumber = (num) => {
-  const trill = 10 ** 11;
+  const trill = 10 ** 12;
   const bill = 10 ** 9;
   const mill = 10 ** 6;
   if (num > trill) return `${roundToDecimal(num / trill)}T`;
--- src/shared.test.js
+++ src/shared.test.js
@@ -1,7 +1,34 @@
 import { roundToDecimal, displayNumber } from "./shared";
 
-test("roundToDecimal adds and removes decimal places", () => {
-  const large = 345989760.89734;
-  const modified = roundToDecimal(large);
-  expect(modified).toEqual("345989760.90");
+const SMALL = 0.00056;
+const MED = 34;
+const LARGE = 475638923.097045;
+const EXTRA = 72998562398476.4;
+
+test("roundToDecimal adds and removes decimal places and commas", () => {
+  const smallMod = roundToDecimal(SMALL);
+  expect(smallMod).toEqual("0.00");
+
+  const medMod = roundToDecimal(MED);
+  expect(medMod).toEqual("34.00");
+
+  const largeMod = roundToDecimal(LARGE);
+  expect(largeMod).toEqual("475,638,923.10");
+
+  const extraMod = roundToDecimal(EXTRA);
+  expect(extraMod).toEqual("72,998,562,398,476.40");
+});
+
+test("displayNumber summarizes very large and very small numbers", () => {
+  const smallMod = displayNumber(SMALL);
+  expect(smallMod).toEqual("<0.01");
+
+  const medMod = displayNumber(MED);
+  expect(medMod).toEqual("34.00");
+
+  const largeMod = displayNumber(LARGE);
+  expect(largeMod).toEqual("475.64M");
+
+  const extraMod = displayNumber(EXTRA);
+  expect(extraMod).toEqual("73.00T");
 });
