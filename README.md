161d206a5aeda134f8e13aa5a72ef4ba197217ff
Jason Offet <joffet@venue.live>
2024-11-10 20:42:59 -0500

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 Seconda Take Home Assignment - Jason Offet
+
+Instructions:
+
+Before running this project, have the node application running first. It is required to enable the API call
+
+1. clone the repo
+2. run npm install
+3. run npm start
--- src/App.test.js
+++ src/App.test.js
@@ -18,3 +18,12 @@ test("renders 5 table headers with the correct titles", () => {
   const columnHeadings = screen.getAllByRole("columnheader");
   expect(columnHeadings).toHaveLength(5);
 });
+
+// test("positive values in the change percentage show green up arrows", () => {
+//   const upArrows = screen.getAllByClass("arrow-up");
+//   upArrows.forEach((e) => {
+//
+//   });
+// });
+
+// ran out of time for this test
